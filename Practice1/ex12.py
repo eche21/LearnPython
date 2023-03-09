8a9701907cd4d27ac3fb36b372e98f1b6709894b
eche21 <ericngangche@yahoo.com>
2023-03-09 10:48:26 -0500

    prompting people

---
+++ Practice1/ex12.py
@@ -0,0 +1,10 @@
+#prompting people
+
+age = input("how old are you?")# This will prompt the user with the age and put the result in the variable age
+height = input("How tall are you?")
+
+print("So, you're %r old and %r tall." % (age,height))
+
+#how old are you? 25
+#How tall are you? 15
+#So, you're ' 25' old and ' 15' tall.
