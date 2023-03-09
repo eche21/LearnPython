0b433f60f7a3041fdc81e0da0d9080c512699455
eche21 <ericngangche@yahoo.com>
2023-03-08 20:30:26 -0500

    using newline characters and three double quotes

---
+++ Practice1/ex9.py
@@ -0,0 +1,22 @@
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApril\nMay\nJun\nJul\nAug"
+
+print("here are the days:", days) #here are the days: Mon Tue Wed Thu Fri Sat Sun
+print("here are the months:", months)
+#here are the months: Jan
+#Feb
+#Mar
+#April
+#May
+#Jun
+#Jul
+#Aug
+
+
+print("""
+There's something going on here
+With the three double-quotes.
+""")
+#There's something going on here
+#With the three double-quotes.
