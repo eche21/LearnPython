6227beed0e23e13a42036878ece9c357f952955c
eche21 <ericngangche@yahoo.com>
2023-03-09 11:26:43 -0500

    using sys lirary, argv module, passing variables and unpack

---
+++ Practice1/ex13.py
@@ -0,0 +1,19 @@
+# Parameters, Unpacking, Variables
+
+
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+# To run the script you must pass three command line arguments
+# For example, python ex13.py first 2nd 3rd
+
+#Output
+#The script is called: ex13.py
+#Your first variable is: first
+#Your second variable is: 2nd
+#Your third variable is: 3rd
