1422fba2c6e72c07b88d9310cfc7132af7f7963c
eche21 <ericngangche@yahoo.com>
2023-03-09 09:35:17 -0500

    input data into a program

--- Practice1/ex10.py
+++ Practice1/ex10.py
@@ -10,16 +10,21 @@ I'll do a list:
 """
 
 my_height= "I am 6'2\" tall."
+
 print(tabby_cat) #I'm tabbed in.
+
 print(persian_cat)
 #I'm split
 #on a line.
+
 print(backslash_cat) #I'm \ a \ cat
+
 print(fat_cat)
 #I'll do a list:
 #	* Cat food
 #	* Fishies
 #	* catnip
 #	* Grass
+
 print(my_height)
 #I am 6'2" tall.
