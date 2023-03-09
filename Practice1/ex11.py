1208f7550c649e50cdd8553799f90b745e54278d
eche21 <ericngangche@yahoo.com>
2023-03-09 09:35:17 -0500

    updating ex10 and input data in a program

---
+++ Practice1/ex11.py
@@ -0,0 +1,15 @@
+#Input data into a program
+#
+print("how old are you?",)
+age = input()
+
+print("How tall are you?",)
+height = input()
+
+print("So, you're %r old and %r tall." % (age,height))
+
+#how old are you?
+#25
+#How tall are you?
+#15
+#So, you're '25' old and '15' tall.
